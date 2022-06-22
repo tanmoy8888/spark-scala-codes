ab67c712bcae665cf0f0a8b64ea3f1aacd16b820
tanmoy8888 <tanmoy8888@yahoo.co.in>
2022-06-22 17:43:43 +0530

    update HigherOrderFunction.scala

--- src/main/scala/functions/HigherOrderFunction.scala
+++ src/main/scala/functions/HigherOrderFunction.scala
@@ -8,13 +8,14 @@ object HigherOrderFunction {
   def sub(x:Int,y:Int):Int = x - y
   def mul(x:Int,y:Int):Int = x * y
   def div(x:Int,y:Int):Int = x / y
+
   def caller(x:Int,y:Int ,f: (Int,Int) => Int):Int = f(x,y)
 
   def main(args: Array[String]): Unit = {
     println(s"add() : ${caller(4,2, add(_,_))}")
-    println(s"add() : ${caller(4,2, sub(_,_))}")
-    println(s"add() : ${caller(4,2, mul(_,_))}")
-    println(s"add() : ${caller(4,2, div(_,_))}")
+    println(s"sub() : ${caller(4,2, sub(_,_))}")
+    println(s"mul() : ${caller(4,2, mul(_,_))}")
+    println(s"div() : ${caller(4,2, div(_,_))}")
   }
 
 }
