baa39359d31143f547d85e54ef5742b01900be95
tanmoy8888 <tanmoy8888@yahoo.co.in>
2022-06-22 17:39:11 +0530

    HigherOrderFunction

---
+++ src/main/scala/functions/HigherOrderFunction.scala
@@ -0,0 +1,20 @@
+package functions
+
+/**
+ * @author Tanmoy Mukherjee
+ */
+object HigherOrderFunction {
+  def add(x:Int,y:Int):Int = x + y
+  def sub(x:Int,y:Int):Int = x - y
+  def mul(x:Int,y:Int):Int = x * y
+  def div(x:Int,y:Int):Int = x / y
+  def caller(x:Int,y:Int ,f: (Int,Int) => Int):Int = f(x,y)
+
+  def main(args: Array[String]): Unit = {
+    println(s"add() : ${caller(4,2, add(_,_))}")
+    println(s"add() : ${caller(4,2, sub(_,_))}")
+    println(s"add() : ${caller(4,2, mul(_,_))}")
+    println(s"add() : ${caller(4,2, div(_,_))}")
+  }
+
+}
